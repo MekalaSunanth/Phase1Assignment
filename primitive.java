602b618b6dcae78098bda4078e76d7867a5c4595
MekalaSunanth <97794619+MekalaSunanth@users.noreply.github.com>
2022-01-16 22:26:08 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,16 @@
+
+public class primitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 ="15";
+		String s2 ="5";
+		int a,b;
+		a=Integer.parseInt(s1);
+		b=Integer.parseInt(s2);
+		int mul = a*b;
+System.out.println("result="+mul);
+
+	}
+
+}
