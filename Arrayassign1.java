843e01aa90d7d2cd523f17eea0155964cd7962cb
MekalaSunanth <97794619+MekalaSunanth@users.noreply.github.com>
2022-01-16 23:26:44 +0530

    Add files via upload

---
+++ Arrayassign1.java
@@ -0,0 +1,11 @@
+package Arrayassign;
+
+public class Arrayassign1 {
+	public static void main(String[] args) {
+		
+		int s[]= {49,59,69,79,89};
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of array s: "+s[i]);
+		}
+	}
+}
---
+++ Arrayassign2.java
@@ -0,0 +1,14 @@
+package Arrayassign;
+
+public class Arrayassign2 {
+	public static void main(String[] args) {
+
+	int[][] b = {
+			      {2, 4, 6, 8}, 
+                  {3, 6, 9} };
+      
+      System.out.println("Length of row 1: " + b[0].length);
+      }
+}
+
+
