a73b78b352d83976e5f43c2a271c2d59b7c3d01e
MekalaSunanth <97794619+MekalaSunanth@users.noreply.github.com>
2022-01-19 23:47:14 +0530

    Add files via upload

---
+++ Developer.java
@@ -0,0 +1,26 @@
+package Trainee;
+class MyException extends Exception{
+	   String str1;
+	   MyException(String str2) {
+		str1=str2;
+	   }
+	   public String toString(){ 
+		return ("MyException Occurred: "+str1) ;
+	   }
+	}
+
+public class Developer {
+
+	   public static void main(String args[]){
+			try{
+				System.out.println("Start of try block");
+				throw new MyException("This is My error Message");
+			}
+			catch(MyException exp){
+				System.out.println("Catch Block") ;
+				System.out.println(exp) ;
+			}
+		   }
+		}
+
+
